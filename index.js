71cd5ae29960e93e9b871f084c00dc9efe3e1dcd
Hendra Manudinata <hendra@manudinata.me>
2022-01-03 08:45:33 +0700

    Add thousands seperator

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 /*
+  Thousands separator
+  Reference:
+  - https://aioos.wordpress.com/2013/07/02/add-thousand-separator-to-value-in-javascript/
+  - https://stackoverflow.com/a/32355056
+*/
+function addThousandsSeparator(no) {
+  return no.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+}
+
+/*
   Parse Indonesia' COVID-19 API with jQuery
   API: https://data.covid19.go.id/public/api/update.json
   Code reference: https://zetcode.com/javascript/jsonurl/
@@ -9,22 +19,22 @@ $.getJSON(API_URL, function (data) {
   // Positives
   var positivesData = `${data.update.total.jumlah_positif}`;
   var positivesElement = document.createElement("th");
-  positivesElement.innerHTML = positivesData;
+  positivesElement.innerHTML = addThousandsSeparator(positivesData);
 
   // Negatives
   var negativesData = `${data.update.total.jumlah_sembuh}`;
   var negativesElement = document.createElement("th");
-  negativesElement.innerHTML = negativesData;
+  negativesElement.innerHTML = addThousandsSeparator(negativesData);
 
   // Deaths
   var deathsData = `${data.update.total.jumlah_meninggal}`;
   var deathsElement = document.createElement("th");
-  deathsElement.innerHTML = deathsData;
+  deathsElement.innerHTML = addThousandsSeparator(deathsData);
 
   // Patient
   var patientData = `${data.update.total.jumlah_dirawat}`;
   var patientElement = document.createElement("th");
-  patientElement.innerHTML = patientData;
+  patientElement.innerHTML = addThousandsSeparator(patientData);
 
   // Process the gathered data
   var htmlTable = document.getElementById("table-data");
