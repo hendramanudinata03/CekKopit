3054f6fc59f86c905be4e52493e1013f07281295
Hendra Manudinata <hendra@manudinata.me>
2022-01-03 21:59:21 +0700

    Add rewind data to table

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ function addThousandsSeparator(no) {
 
 /*
   Parse Indonesia' COVID-19 API with jQuery
-  API: https://apicovid19indonesia-v2.vercel.app/api/indonesia
+  API:
+  - https://apicovid19indonesia-v2.vercel.app/api/indonesia
+  - https://apicovid19indonesia-v2.vercel.app/api/indonesia/harian
   Code reference: https://zetcode.com/javascript/jsonurl/
 */
-var API_URL = "https://apicovid19indonesia-v2.vercel.app/api/indonesia";
+var LATEST_API_URL = "https://apicovid19indonesia-v2.vercel.app/api/indonesia";
+var MORE_API_URL = "https://apicovid19indonesia-v2.vercel.app/api/indonesia/harian";
 
-$.getJSON(API_URL, function (data) {
+// #data-table
+$.getJSON(LATEST_API_URL, function (data) {
   // Positives
   var positivesData = `${data.positif}`;
   var positivesElement = document.createElement("th");
@@ -47,4 +51,46 @@ $.getJSON(API_URL, function (data) {
   var lastUpdateData = `${data.lastUpdate}`;
   var captionTable = document.getElementById("table-caption");
   captionTable.innerHTML += "(" + lastUpdateData + ")";
+});
+
+// #rewind-data-table
+$.getJSON(MORE_API_URL, function (data) {
+  for (let i = 1; i < 8; i++) {
+    // Positives
+    var positivesRewindData = `${data[data.length - i].positif_kumulatif}`;
+    var positivesRewindElement = document.createElement("th");
+    positivesRewindElement.innerHTML = addThousandsSeparator(positivesRewindData);
+
+    // Negatives
+    var negativesRewindData = `${data[data.length - i].sembuh_kumulatif}`;
+    var negativesRewindElement = document.createElement("th");
+    negativesRewindElement.innerHTML = addThousandsSeparator(negativesRewindData);
+
+    // Deaths
+    var deathsRewindData = `${data[data.length - i].meninggal_kumulatif}`;
+    var deathsRewindElement = document.createElement("th");
+    deathsRewindElement.innerHTML = addThousandsSeparator(deathsRewindData);
+
+    // Patients
+    var patientRewindData = `${data[data.length - i].dirawat_kumulatif}`;
+    var patientRewindElement = document.createElement("th");
+    patientRewindElement.innerHTML = addThousandsSeparator(patientRewindData);
+
+    // Last updates
+    var lastUpdateRewindData = `${data[data.length - i].tanggal}`;
+    var lastUpdateRewindElement = document.createElement("th");
+    lastUpdateRewindElement.innerHTML = lastUpdateRewindData;
+
+    // Process the gathered data
+    var tableRewindEntries = document.createElement("tr");
+    tableRewindEntries.appendChild(positivesRewindElement);
+    tableRewindEntries.appendChild(negativesRewindElement);
+    tableRewindEntries.appendChild(deathsRewindElement);
+    tableRewindEntries.appendChild(patientRewindElement);
+    tableRewindEntries.appendChild(lastUpdateRewindElement);
+
+    // Append all `<tr>` to `<tbody>`
+    var htmlTable = document.getElementById("rewind-table-body");
+    htmlTable.appendChild(tableRewindEntries);
+  }
 });
